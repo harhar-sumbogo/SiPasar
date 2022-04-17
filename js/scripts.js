a3f5ed06cb574e44e3f357cd8f40f185e0b00809
kikazuru <harriskurniadi2@gmail.com>
2022-04-17 23:17:21 +0700

    sidebar fixed position

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@ window.addEventListener('DOMContentLoaded', event => {
         sidebarToggle.addEventListener('click', event => {
             event.preventDefault();
             document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+            // localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
         });
     }
 
